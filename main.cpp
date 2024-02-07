dff0aeebc10fa584e979cce032e6d68d50b4f4a2
protarie125 <protarie@gmail.com>
2024-02-07 21:21:01 +0900

    초기 커밋.

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include <bit>
+#include <chrono>
+#include <cmath>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <ranges>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using pll = pair<ll, ll>;
+using vll = vector<pll>;
+using vs = vector<string>;
+
+int main() {
+  ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  string find = "UCPC";
+  int idx = 0;
+  string s;
+
+  getline(cin, s);
+
+  for (const auto& x : s) {
+    if (x == find[idx]) {
+      ++idx;
+      if (idx == 4) {
+        cout << "I love UCPC";
+        return 0;
+      }
+    }
+  }
+
+  cout << "I hate UCPC";
+
+  return 0;
+}
